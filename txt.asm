5f08a2ed5b126586ec7008308b252a080699a780
li3zhi4 <li3zhi4@163.com>
2020-05-03 23:18:20 +0800

    Update txt.asm

--- txt.asm
+++ txt.asm
@@ -1,5 +1,5 @@
 ;显示静态文字
-org 07c00h  ; cs:ip -> 0h:7c00h
+    org 07c00h ; cs:ip -> 0h:7c00h
     mov ax, cs
     mov ds, ax ; ds -> cs
     mov es, ax ; es -> cs
@@ -10,8 +10,8 @@ display:
     mov bp, ax ; bp -> msg.location
     mov cx, msgend-msgstart ; cx -> msg.length
     mov ax, 01301h ; ah=13h, al=01h
-    mov bx, 000ch ; bh -> page, bl -> value 1. 0 black 1 blue 2. c red
-    mov dx, 0D20h ; dh -> row, dl -> colum
+    mov bx, 000ch ; bh -> page, bl -> attribute
+    mov dx, 0D20h ; dh -> row, dl -> column
     int 10h ; -> Video Service es:bp
     ret ; return
 msgstart:
